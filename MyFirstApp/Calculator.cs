302a87846fc860b12df016712b46b413e8b7a8fd
DESKTOP-L00RSOR\Sergio Rebolledo <sergio200796@hotmail.com>
2021-06-25 10:48:46 +0200

    Initial commit with calculator example.

---
+++ MyFirstApp/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    public class Calculator
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Substract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public int Multiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public int Divide(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
